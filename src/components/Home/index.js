d4e426bcd536df33378e36c823d998ff15acea7d
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-05-19 21:34:29 +0500

    Navbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 const ThisForTest = () => {
-  return <div>ThisForTest
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-      <h1>dsjfksdjnfksdjfnskdjn</h1>
-  </div>;
+  return (
+    <div>
+      ThisForTest
+      <h1>This is sample Component</h1>
+    </div>
+  );
 };
 
 export default ThisForTest;
