17d4314fc184f064102eedc81feee310a7980f99
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-05-19 18:54:55 +0500

    test

---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ThisForTest = () => {
+  return <div>ThisForTest
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+      <h1>dsjfksdjnfksdjfnskdjn</h1>
+  </div>;
+};
+
+export default ThisForTest;
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ThisForTest from "../components/Home";
 import Navbar from "../components/Navbar";
 import { Container } from "./style";
 
@@ -6,6 +7,7 @@ const Root = () => {
   return (
     <Container>
       <Navbar />
+      <ThisForTest />
     </Container>
   );
 };
